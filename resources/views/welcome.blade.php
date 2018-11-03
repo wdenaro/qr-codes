9ca886070675b695bea43c8f39f4fa38e95ac823
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-11-02 22:12:31 -0400

    a few tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.7.9
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ Route::get('form', function () {
     return view('form');
     })->name('form.show');
 
-Route::post('qr', 'mainController@index')
+
+Route::post('qr', 'MainController@index')
     ->name('qr.show');
+
 
 Route::get('/', function () {
     return view('welcome');
